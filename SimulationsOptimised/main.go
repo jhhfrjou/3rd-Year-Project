301c1f12244f1ed7dab8387ddc1a0f9d0a1c34c2
jhhfrjou <jhhfrjou1@gmail.com>
2019-03-04 15:28:03 +0000

    new build

--- SimulationsOptimised/main.go
+++ SimulationsOptimised/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"sync"
+	"time"
 )
 
 func main() {
@@ -27,12 +27,9 @@ func allScens() {
 }
 
 func singleScen(scen scenario) {
-	iters := 100
+	iters := 10000
 	/*
-		allocs, _ := hillClimb(iters, scen, timeO)
-		timeO, _ := time.ParseDuration("5m")
 		fmt.Println("Anneal")
-		prettyPrintAllocation(allocs[len(allocs)-1])
 		alloc := readAllocFromFile("allocations/1stTestAscend.json")
 		fmt.Println("Hill Climb")
 		prettyPrintAllocation(allocs[len(allocs)-1])
@@ -40,18 +37,23 @@ func singleScen(scen scenario) {
 		alloc := getRandomWeight(scen)
 		fmt.Println("Genetic")
 		allocs[index] = anneal(iters, 100, float64(10*index), scen)
+		allocs = geneticAlgo(iters, 1000, scen, 1)
+		prettyPrintAllocation(allocs[len(allocs)-1])
 	*/
-	allocs := make([][]allocation, 10)
+	allocs := make([][]allocation, 1)
+	//allocs := ascend(iters, 0.0001, 0.0001, scen)
+
 	wg := sync.WaitGroup{}
+	timeO, _ := time.ParseDuration("1h")
 	wg.Add(10)
 	for i := 0; i < 10; i++ {
 		go func(index int) {
-			allocs[index] = geneticAlgo(iters, 1000, scen, 0.1*float64(index))
-			fmt.Println(index, allocs[index][iters-1].Score)
+			allocs, _, _ := hillClimb(iters, scen, timeO)
+			prettyPrintAllocation(allocs[len(allocs)-1])
 			wg.Done()
 		}(i)
 	}
 	wg.Wait()
-	writeManyToCSV(allocs, "MutationFactor3.csv")
+	writeManyToCSV(allocs, "hillClimb3.csv")
 
 }
