ea88abea4ad6e65cd9d6e704d526688150e35380
jhhfrjou <jhhfrjou1@gmail.com>
2019-03-18 22:26:26 +0000

    and the script

--- SimulationsOptimised/getResults.sh
+++ SimulationsOptimised/getResults.sh
@@ -2,4 +2,3 @@
  ssh ug 'scp cs:~/3rd-Year-Project/SimulationsOptimised/results/*Run*.csv ~/'
  scp ug:~/*Run*.csv ~/Dropbox/Uni/3rd\ Year/1\ Dis/SimulationsOptimised/results
  ssh ug 'rm ~/*Run*.csv'
- python *.py
